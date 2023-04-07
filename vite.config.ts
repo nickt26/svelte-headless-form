da6de77b50544e5e74fe26b241f522d36f7e2436
Nicholas Trummer <nick2697@gmail.com>
2023-04-07 21:02:15 +1000

    Add code bundling for mjs and cjs outputs with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
+import { resolve } from 'path';
 import type { UserConfig } from 'vite';
 
-const config: UserConfig = {};
+const config: UserConfig = {
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'dist/index.js'),
+			name: 'svelte-headless-form',
+			fileName: 'svelte-headless-form'
+		}
+	}
+};
 
 export default config;
